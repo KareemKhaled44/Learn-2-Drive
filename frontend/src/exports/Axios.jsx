ef52051476b6d777ad2d4668e1a7bbaa83286850
AmrKhalidMohamed <amrkfawzy@gmail.com>
2026-09-15 02:18:34 +0300

    Configure frontend for production

--- frontend/src/exports/Axios.jsx
+++ frontend/src/exports/Axios.jsx
@@ -1,7 +1,7 @@
 // frontend/src/exports/Axios.jsx
 import axios from "axios";
 
-const baseURL = "http://localhost:8000/";
+const baseURL =  import.meta.env.VITE_API_URL || "http://localhost:8000/api/";
 const api = axios.create({
   baseURL,
   headers: {

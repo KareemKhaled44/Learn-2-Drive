c64c8280e6d70bcdbadf42a5d09bb0ec68a46caa
AmrKhalidMohamed <amrkfawzy@gmail.com>
2026-09-16 00:39:40 +0300

    Configure frontend for production

--- backend/bookings/serializers.py
+++ backend/bookings/serializers.py
@@ -116,7 +116,6 @@ class BookingCreateSerializer(serializers.ModelSerializer):
 
             booking = Booking.objects.create(**validated_data)
 
-            transaction.on_commit(lambda: send_booking_confirmation_email(booking))
 
         return booking
 
